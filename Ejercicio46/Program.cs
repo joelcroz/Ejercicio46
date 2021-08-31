1852dbf9374d236efce83881418be21ead3a73bc
Joelcrosz <joelcisneros528@gmail.com>
2021-08-31 16:42:51 -0300

    Ejercicio finalizado

--- Ejercicio46/Program.cs
+++ Ejercicio46/Program.cs
@@ -10,6 +10,19 @@ namespace Ejercicio46
     {
         static void Main(string[] args)
         {
-        }
+            int cantidad = 0;
+            var suma = 0;
+            do
+            {
+				Console.WriteLine("Ingrese un nùmero:");
+				var numeroIngresado = Convert.ToInt32(Console.ReadLine());
+				suma += numeroIngresado;
+				cantidad++;
+            } while (suma<=60);
+
+			Console.WriteLine($"Se ingresaron {cantidad} de nùmeros");
+			Console.WriteLine($"La suma de los mismos es {suma}");
+			Console.ReadLine();
+		}
     }
 }
